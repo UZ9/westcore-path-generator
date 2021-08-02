a82068677c6dc3700933106c9dbc8f985ad54d60
Yerti <36551149+UZ9@users.noreply.github.com>
2021-08-02 18:27:53 -0500

    :bug: Fix name and position not updating in node store

--- src/stores/NodeStore.jsx
+++ src/stores/NodeStore.jsx
@@ -1,6 +1,8 @@
 import create from 'zustand'
+import produce from 'immer'
 
 export const useNodeStore = create(set => ({
     nodes: [],
+    setNodeState: (fn) => set(produce(fn)),
     addNode: (node) => {set(state => ({ nodes: [...state.nodes, node] })); console.log("Setting node")}
 }))

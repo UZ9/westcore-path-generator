951794a201e4d1a79f9199dec5ee71c779873da4
Yerti <36551149+UZ9@users.noreply.github.com>
2021-08-10 20:26:10 -0500

    🧹 Cleanup robot visualization code

--- src/ui/NodeConnection.jsx
+++ src/ui/NodeConnection.jsx
@@ -58,7 +58,6 @@ export default function NodeConnection({ dragging, setDragging, model, startMark
             })
 
             eventControls.attachEvent('onclick', function (event) {
-
                 if (event.altKey)
                     model.current.material = M.tileGridMat(fragmentShader, vertexShader)
             })
@@ -84,9 +83,6 @@ export default function NodeConnection({ dragging, setDragging, model, startMark
 
                     this.focused.position.x = 11.855 * Math.round((this.focused.position.x) / 11.855);
                     this.focused.position.z = 11.855 * Math.round((this.focused.position.z) / 11.855);
-                } else {
-                    // Update marker position to wherever the mouse pointer is currently located
-                    // this.focused.position.y = this.previous.y;
                 }
 
                 if (this.focused.name === "v0") {
@@ -95,9 +91,6 @@ export default function NodeConnection({ dragging, setDragging, model, startMark
                 } else {
                     setVectors([vectorRef.current[0], new THREE.Vector2(this.focused.position.x, this.focused.position.z)]);
                 }
-                // }
-
-
             });
 
             eventControls.attach(v0Mesh.current);
@@ -106,45 +99,14 @@ export default function NodeConnection({ dragging, setDragging, model, startMark
             eventControls.map = model.current;
         }
 
-
-
-
-
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [camera, gl.domElement, model, dragging, setDragging])
     let points = curve.getPoints(400);
 
-    // const colors = [];
-
-    // const color = new THREE.Color();
-
-    // const n = 25;
-
-    // for (let i = 0; i < n; i++) {
-    //     color.setRGB(i / 50, i / 50, i / 50);
-
-    //     colors.push(color.r, color.g, color.b);
-    // }
-
     const geometry = new THREE.BufferGeometry().setFromPoints(points.map((v, i) => new THREE.Vector2(v[0], v[1])));
-
-    // const robotGeometry = new THREE.BufferGeometry().setFromPoints(points.map((v, i) => new THREE.Vector2(v[0] * 1.5, v[1] * 1.5)));
-
 
     const v0Geometry = new THREE.BufferGeometry().setFromPoints([startPos, new THREE.Vector3(vectors[0].x, 0, vectors[0].y)]);
     const v1Geometry = new THREE.BufferGeometry().setFromPoints([endPos, new THREE.Vector3(vectors[1].x, 0, vectors[1].y)]);
-
-    // const straightLineGeometry = new THREE.BufferGeometry().setFromPoints(
-    //     [
-    //         new THREE.Vector2(-5, 0.1),
-    //         new THREE.Vector2(5, 0.1),
-    //         new THREE.Vector2(5, 6.5),
-    //         new THREE.Vector2(-5, 6.5),
-    //         new THREE.Vector2(-5, 0.1)
-    //     ]);
-
-    // geometry.setAttribute('color', new THREE.Float32BufferAttribute(colors, 3));
-
 
     const visualizationDrawRate = 24;
 
@@ -155,7 +117,6 @@ export default function NodeConnection({ dragging, setDragging, model, startMark
 
     return (
         <>
-
             <mesh name={"v0"} ref={v0Mesh} position={[vectors[0].x, 0, vectors[0].y]}>
                 <boxGeometry args={[1, 1, 1]} />
                 <meshStandardMaterial color={"red"} />
@@ -166,29 +127,17 @@ export default function NodeConnection({ dragging, setDragging, model, startMark
                 <meshStandardMaterial color={"green"} />
             </mesh>
 
-
             <line position={[0, 0.1, 0]} rotation={[Math.PI / 2, 0, 0]} geometry={geometry} />
             {robotVisualization ? points.slice(0, -1).filter((_element, index) => index % visualizationDrawRate === 0).map((v, i) => {
-
                 i = i * visualizationDrawRate;
 
-                console.log([v, points[i + 1], Math.atan2(v[1] - points[i + 1][1], v[0] - points[i + 1][0])]);
-
                 return (
-                    <>
-                        {/* <line key={"straightline" + i} position={[v[0], 0.1, v[1]]} geometry={straightLineGeometry}>
-                            <lineBasicMaterial color={"red"} />
-
-                        </line> */}
-                        <mesh rotation={[0, Math.atan2(v[0] - points[i + 1][0], v[1] - points[i + 1][1]), 0]} position={[points[i + 1][0], 0, points[i + 1][1]]} >
-                            <boxGeometry args={[robotWidth, robotHeight, robotDepth]}/>
-                            <meshStandardMaterial color={"red"} depthWrite={false} depthTest={false} opacity={0.2} transparent={true} /> */
-                        </mesh>
-                    </>
+                    <mesh rotation={[0, Math.atan2(v[0] - points[i + 1][0], v[1] - points[i + 1][1]), 0]} position={[points[i + 1][0], 0, points[i + 1][1]]} >
+                        <boxGeometry args={[robotWidth, robotHeight, robotDepth]} />
+                        <meshStandardMaterial color={"red"} depthWrite={false} depthTest={false} opacity={0.2} transparent={true} /> */
+                    </mesh>
                 )
             }) : <></>}
-            {/* <line position={[0, 0.1, 0]} rotation={[Math.PI / 2, 0, 0]} geometry={robotGeometry} /> */}
-            {/* <line position={[0, 0.1, -3]} rotation={[Math.PI / 2, 0, 0]} geometry={geometry} /> */}
 
             <line position={[0, 0.1, 0]} geometry={v0Geometry}>
                 <lineBasicMaterial color={"yellow"} />
@@ -197,28 +146,5 @@ export default function NodeConnection({ dragging, setDragging, model, startMark
             <line position={[0, 0.1, 0]} geometry={v1Geometry}>
                 <lineBasicMaterial color={"yellow"} />
             </line>
-
-            {/* {points.map((v, i) => {
-                return <mesh position={[v.x, 0, v.y]}><boxGeometry args={[0.3, 0.3, 0.3]} /><meshStandardMaterial /></mesh>
-            })} */}
-            {/* <mesh position={startPos}>
-                <boxGeometry args={[1, 1, 1]}/>
-                <meshStandardMaterial color={"red"}/>
-            </mesh> */}
-            {/* <mesh
-                rotation={[Math.PI / 2, 0, -m]} // Rotation is in radians, X is rotated to be aligned with the field
-                position={startPos}
-                geometry={getGeometry()}
-            // material={this.getMaterial()}
-            // color={selected ? 'green' : hovered ? 'yellow' : 'blue'}
-            // onClick={(event) => this.onClick(event)}
-            // onPointerOver={(_event) => this.setState(() => ({ selected: this.state.selected, hovered: true }))}
-            // onPointerOut={(_event) => this.setState(() => ({ selected: this.state.selected, hovered: false }))}
-            >
-                <meshStandardMaterial metalness={0.5} transparent={true} opacity={1} color={props.color} />
-
-            </mesh> */}
-
-
         </>)
 }
